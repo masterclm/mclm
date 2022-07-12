6da72a7b566b04e839b9d3b86459849b0c51f257
mariana <mariana.montes@kuleuven.be>
2022-07-12 10:26:32 +0200

    roxygenize private functions in config.R

--- R/config.R
+++ R/config.R
@@ -1,23 +1,21 @@
-# ----------------------------------------------------------------------------
-# implementation of the use of YAML-based config files in mclm
-# ----------------------------------------------------------------------------
-# namespace dependencies:
-#   readr::read_lines()
-#   readr::write_lines()
-#   yaml::yaml.load()
-#   yaml::as.yaml()
-# ----------------------------------------------------------------------------
-
-# filename: filename of either the corresponding data set (a csv file)
-#                      or the actual config file (a yaml file)
+#' Generate filename for a config file
+#'
+#' @param filename Character string
+#'
+#' @return Character string
+#' @noRd
 config_filename <- function(filename) {
   ret_val <- gsub("(?xi)  [.] [^.]+ $", "", filename, perl = TRUE)
   ret_val <- paste0(ret_val, ".yaml")
   ret_val
 }
 
-# filename: filename of either the corresponding data set (a csv file)
-#                       or the actual config file (a yaml file)
+#' Read configuration file
+#'
+#' @param filename Character string.
+#'
+#' @return Contents of configuration file
+#' @noRd
 read_config <- function(filename) {
   ret_val <- NULL
   c_fname <- config_filename(filename)
@@ -31,13 +29,17 @@ read_config <- function(filename) {
 # x: a list with config features and values
 # filename: filename of either the corresponding data set (a csv file)
 #                       or the actual config file (a yaml file)
+#' Write configuration file
+#'
+#' @param x  List of config features and values
+#' @param filename Either the corresponding data set (a csv file) or the actual
+#'   config file (a yaml file).
+#'
+#' @return Invisibly, \code{x}.
+#' @noRd
 write_config <- function(x, filename) {
   c_fname <- config_filename(filename)
   c_lines <- yaml::as.yaml(x)
   readr::write_lines(c_lines, c_fname)
   invisible(x)
 }
-
-
-
-
