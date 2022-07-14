dc42df9db42920615f300ac6669ac50bbac7b27b
mariana <mariana.montes@kuleuven.be>
2022-07-14 18:58:47 +0200

    add keywords tutorial and custom learnr functions in package. Still, must review, because we need too much data (do we?)

---
+++ R/learnr.R
@@ -0,0 +1,44 @@
+#' Learnr question wrappers with desired defaults
+#' 
+#' These are wrappers for [learnr::question()] and related functions that
+#' always allow retries, give answers in a random order, and use
+#' [learnr::random_praise()] and encouragement for the reactions.
+#' To be used in tutorials.
+#'
+#' @param ... A question and a series of [learnr::answer()].
+#' @param q A question.
+#' @param a The correct answer.
+#'
+#' @return A [learnr::question()] with desired defaults.
+#' @export
+#' @keywords internal
+mclm_question <- function(...){
+  learnr::question(...,
+           allow_retry = TRUE,
+           random_answer_order = TRUE,
+           correct = learnr::random_praise(),
+           incorrect = learnr::random_encouragement()
+  )
+} 
+
+#' @rdname mclm_question
+#' @export
+mclm_question_num <- function(q, a) {
+  learnr::question_numeric(q,
+                           learnr::answer(a, correct = TRUE),
+                           allow_retry = TRUE,
+                           correct = learnr::random_praise(),
+                           incorrect = learnr::random_encouragement(),
+                           min = 5000,
+                           max = 900000,
+                           step = 100)
+}
+
+#' @rdname mclm_question
+#' @export
+mclm_question_text <- function(...) {
+  learnr::question_text(...,
+                allow_retry = TRUE,
+                correct = learnr::random_praise(),
+                incorrect = learnr::random_encouragement())
+}
