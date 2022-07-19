3abbd76298b66333a318dd43e2e52bbd36f80fd0
Mariana Montes <mariana.montes@kuleuven.be>
2022-07-19 19:25:43 +0200

    start reorganization of re.R

--- R/re.R
+++ R/re.R
@@ -1,31 +1,259 @@
-# =============================================================================
-# the class "re"
-# =============================================================================
+# Constructor ==================================================================
 
-# to be exported:
-#    re_retrieve_first()
-#    re_retrieve_last()
-#    re_retrieve_all()
-#    re_has_matches()
-#    re_which()
-#    re_replace_first()
-#    re_replace_all()
-#    
-#    re()                # constructor 
-#    print.re()          # S3 Method
-#    plot.re()           # S3 Method (stub)
-#    summary.re()        # S3 Method
-#    print.summary.re()  # S3 Method
-#    plot.summary.re()   # S3 Method (stub)
-#    perl_flavor
-#    perl_flavor<-
+re <- function(x, perl = TRUE, ...) {
+  # -- test x for errors
+  if (!"character" %in% class(x)) {
+    stop("x must be of the class 'character'")
+  }
+  if (is.na(x[1])) {
+    stop("x[1] must not be NA")
+  }
+  # -- test perl for errors
+  if (!is.logical(perl)) {
+    stop("perl must be a logical vector")
+  }
+  if (is.na(perl[1])) {
+    stop("perl[1] must not be NA")
+  }  
+  # -- test x for peculiarities we want to warn about
+  if (length(x) > 1) {
+    warning("x contains multiple items; only x[1] is used")
+  }
+  # -- test perl for peculiarities we want to warn about
+  if (length(perl) > 1) {
+    warning("perl contains multiple items; only perl[1] is used")
+  }
+  # -- test ... argument
+  extra_args <- names(list(...))
+  if ("invert" %in% extra_args) {
+    warning("unsupported invert argument is ignored")
+  }
+  # construct 're' object
+  result <- list(x = x[1], perl = perl[1])
+  class(result) <- "re"
+  # return result
+  result
+}
 
-#    as.character.re()   # S3 Method
-#    as_character.re()   # S3 Method
+as_re <- function(x, perl = TRUE, ...) {
+  re(x, perl = perl, ...)
+}
 
-# dependencies:
+# not a big fan of the following notation, but supporting it anyway
+# IDEA remove?
+as.re <- function(x, perl = TRUE, ...) {
+  re(x, perl = perl, ...)
+}
 
-# -- helper functions ---------------------------------------------------------
+# Public functions applied to the class ========================================
+
+perl_flavor <- function(x) {
+  if (!"re" %in% class(x)) {
+    stop("x must be an object of the class 're'")
+  }
+  x$perl
+}
+
+`perl_flavor<-` <- function(x, value) {
+  if (!"re" %in% class(x)) {
+    stop("x must be an object of the class 're'")
+  }
+  x$perl <- value
+  x
+}
+
+# S3 Methods from other packages ===============================================
+print.re <- function(x, ...) {
+  if (!"re" %in% class(x)) {
+    stop("x must be an object of the class 're'")
+  }
+  cat("Regular expression (perl = ", x$perl, ")\n", sep = "")
+  cat("------------------\n")
+  cat(x$x, "\n")
+}
+
+plot.re <- function(x, ...) {
+  if (!"re" %in% class(x)) {
+    stop("x must be an object of the class 're'")
+  }
+  warning("there is no plot method for 're' objects; doing nothing")
+  invisible(NULL)
+}
+
+as.character.re <- function(x, ...) {
+  if (!"re" %in% class(x)) {
+    stop("x must be an object of the class 're'")
+  }
+  x$x
+}
+
+as_character.re <- function(x, ...) {
+  if (!"re" %in% class(x)) {
+    stop("x must be an object of the class 're'")
+  }
+  x$x
+}
+
+## Summary ---------------------------------------------------------------------
+summary.re <- function(object, ...) {
+  if (!"re" %in% class(object)) {
+    stop("object must be an object of the class 're'")
+  }
+  result <- object
+  class(result) <- c("summary.re", "re")
+  result
+}
+
+print.summary.re <- function(x, ...) {
+  if (!"summary.re" %in% class(x)) {
+    stop("x must be an object of the class 'summary.re'")
+  }
+  cat("Regular expression (perl = ", x$perl, ")\n", sep = "")
+  cat("------------------\n")
+  cat(x$x, "\n")
+}
+
+plot.summary.re <- function(x, ...) {
+  if (!"summary.re" %in% class(x)) {
+    stop("x must be an object of the class 'summary.re'")
+  }
+  warning("there is no plot method for 'summary.re' objects; doing nothing")
+  invisible(NULL)
+}
+
+# Public regex convenience functions ===========================================
+
+re_retrieve_first <- function(x, pattern, 
+                              ignore.case = FALSE, perl = TRUE,
+                              fixed = FALSE, useBytes = FALSE, 
+                              requested_group = NULL,
+                              drop_NA = FALSE,
+                              ...) {
+  if (is.null(x)) {
+    stop('argument x must not be NULL')
+  }
+  m <- lapply(x, gregexpr, pattern = pattern,  
+              ignore.case = ignore.case, perl = perl,
+              fixed = fixed, useBytes = useBytes)
+  if (is.null(requested_group) || requested_group == 0) {
+    result <- Map(function(x, m) h_regmatches(x, m)[[1]], 
+                  x, m)
+  } else if (length(m) > 0 &&
+             requested_group >= 1 &&
+             requested_group <= length(attr(m[[1]][[1]], 'capture.start')[1,])) {
+    # in following line, changed m to m[[1]] on 2020-11-11
+    # but switched back to m on 2021-07-29
+    result <- Map(function(x, m) h_regmatchesgroup(x, m,
+                                                   group = requested_group)[[1]], 
+                  x, m)
+  } else {
+    result <- rep(NA, length(x))
+  }
+  names(result) <- NULL
+  result <- unlist(lapply(result, h_get_first))
+  if (drop_NA) {
+    result <- result[!is.na(result)]
+  }
+  result
+}
+
+re_retrieve_last <- function(x, pattern, 
+                             ignore.case = FALSE, perl = TRUE,
+                             fixed = FALSE, useBytes = FALSE, 
+                             requested_group = NULL,
+                             drop_NA = FALSE,
+                             ...) {
+  if (is.null(x)) {
+    stop('argument x must not be NULL')
+  }
+  m <- lapply(x, gregexpr, pattern = pattern,  
+              ignore.case = ignore.case, perl = perl,
+              fixed = fixed, useBytes = useBytes)
+  if (is.null(requested_group) || requested_group == 0) {
+    result <- Map(function(x, m) h_regmatches(x, m)[[1]], 
+                  x, m)
+  } else if (length(m) > 0 &&
+             requested_group >= 1 &&
+             requested_group <= length(attr(m[[1]][[1]], 'capture.start')[1,])) {
+    # in following line, changed m to m[[1]] on 2020-11-11
+    # but switched back to m on 2021-07-29
+    result <- Map(function(x, m) h_regmatchesgroup(x, m,
+                                                   group = requested_group)[[1]], 
+                  x, m)
+  } else {
+    result <- rep(NA, length(x))
+  }
+  names(result) <- NULL
+  result <- unlist(lapply(result, h_get_last))
+  if (drop_NA) {
+    result <- result[!is.na(result)]
+  }
+  result
+}
+
+re_retrieve_all <- function(x, pattern, 
+                            ignore.case = FALSE, perl = TRUE,
+                            fixed = FALSE, useBytes = FALSE,
+                            requested_group = NULL,
+                            unlist = TRUE,
+                            ...) {
+  if (is.null(x)) {
+    stop('argument x must not be NULL')
+  }
+  m <- lapply(x, gregexpr, pattern = pattern,  
+              ignore.case = ignore.case, perl = perl,
+              fixed = fixed, useBytes = useBytes)
+  if (is.null(requested_group) || requested_group == 0) {
+    result <- Map(function(x, m) h_regmatches(x, m)[[1]], 
+                  x, m)
+  } else if (length(m) > 0 &&
+             requested_group >= 1 &&
+             requested_group <= length(attr(m[[1]][[1]], 'capture.start')[1,])) {
+    # in following line, changed m to m[[1]] on 2020-11-11
+    # but switched back to m on 2021-07-29
+    result <- Map(function(x, m) h_regmatchesgroup(x, m,
+                                                   group = requested_group)[[1]], 
+                  x, m)
+  } else {
+    result <- rep(NA, length(x))
+  }
+  names(result) <- NULL
+  if (unlist) { result <- unlist(result) }
+  result
+}
+
+re_has_matches <- function(x, pattern, 
+                           ignore.case = FALSE, perl = TRUE,
+                           fixed = FALSE, useBytes = FALSE, 
+                           ...) {
+  grepl(pattern, x, ignore.case = ignore.case, perl = perl,
+        fixed = fixed, useBytes = useBytes)
+}
+
+re_which <- function(x, pattern, 
+                     ignore.case = FALSE, perl = TRUE,
+                     fixed = FALSE, useBytes = FALSE, 
+                     ...) {
+  grepl(pattern, x, ignore.case = ignore.case, perl = perl,
+        fixed = fixed, useBytes = useBytes)
+}
+
+re_replace_first <- function(x, pattern, replacement,
+                             ignore.case = FALSE, perl = TRUE,
+                             fixed = FALSE, useBytes = FALSE, 
+                             ...) {
+  sub(pattern, replacement, x, ignore.case = ignore.case, 
+      perl = perl, fixed = fixed, useBytes = useBytes)
+}
+
+re_replace_all <- function(x, pattern, replacement,
+                           ignore.case = FALSE, perl = TRUE,
+                           fixed = FALSE, useBytes = FALSE, ...) {
+  gsub(pattern, replacement, x, ignore.case = ignore.case, 
+       perl = perl, fixed = fixed, useBytes = useBytes)
+}
+
+# Private helper functions =====================================================
 
 # regcapturedmatches.R
 # https://gist.github.com/GegznaV/57b4ff13e6d7a8a8344e
@@ -179,262 +407,9 @@ h_strip <- function (x, regex = "^\\s+|\\s+$", perl = TRUE) {
   gsub(regex, "", x, perl = perl)
 }
 
-# ---- public regex convenience functions ------------------------------
 
-re_retrieve_first <- function(x, pattern, 
-                              ignore.case = FALSE, perl = TRUE,
-                              fixed = FALSE, useBytes = FALSE, 
-                              requested_group = NULL,
-                              drop_NA = FALSE,
-                              ...) {
-  if (is.null(x)) {
-    stop('argument x must not be NULL')
-  }
-  m <- lapply(x, gregexpr, pattern = pattern,  
-              ignore.case = ignore.case, perl = perl,
-              fixed = fixed, useBytes = useBytes)
-  if (is.null(requested_group) || requested_group == 0) {
-    result <- Map(function(x, m) h_regmatches(x, m)[[1]], 
-                  x, m)
-  } else if (length(m) > 0 &&
-        requested_group >= 1 &&
-        requested_group <= length(attr(m[[1]][[1]], 'capture.start')[1,])) {
-    # in following line, changed m to m[[1]] on 2020-11-11
-    # but switched back to m on 2021-07-29
-    result <- Map(function(x, m) h_regmatchesgroup(x, m,
-                                                   group = requested_group)[[1]], 
-                  x, m)
-  } else {
-    result <- rep(NA, length(x))
-  }
-  names(result) <- NULL
-  result <- unlist(lapply(result, h_get_first))
-  if (drop_NA) {
-    result <- result[!is.na(result)]
-  }
-  result
-}
-
-re_retrieve_last <- function(x, pattern, 
-                             ignore.case = FALSE, perl = TRUE,
-                             fixed = FALSE, useBytes = FALSE, 
-                             requested_group = NULL,
-                             drop_NA = FALSE,
-                             ...) {
-  if (is.null(x)) {
-    stop('argument x must not be NULL')
-  }
-  m <- lapply(x, gregexpr, pattern = pattern,  
-              ignore.case = ignore.case, perl = perl,
-              fixed = fixed, useBytes = useBytes)
-  if (is.null(requested_group) || requested_group == 0) {
-    result <- Map(function(x, m) h_regmatches(x, m)[[1]], 
-                  x, m)
-  } else if (length(m) > 0 &&
-             requested_group >= 1 &&
-             requested_group <= length(attr(m[[1]][[1]], 'capture.start')[1,])) {
-    # in following line, changed m to m[[1]] on 2020-11-11
-    # but switched back to m on 2021-07-29
-    result <- Map(function(x, m) h_regmatchesgroup(x, m,
-                                                   group = requested_group)[[1]], 
-                  x, m)
-  } else {
-    result <- rep(NA, length(x))
-  }
-  names(result) <- NULL
-  result <- unlist(lapply(result, h_get_last))
-  if (drop_NA) {
-    result <- result[!is.na(result)]
-  }
-  result
-}
-
-re_retrieve_all <- function(x, pattern, 
-                           ignore.case = FALSE, perl = TRUE,
-                           fixed = FALSE, useBytes = FALSE,
-                           requested_group = NULL,
-                           unlist = TRUE,
-                           ...) {
-  if (is.null(x)) {
-    stop('argument x must not be NULL')
-  }
-  m <- lapply(x, gregexpr, pattern = pattern,  
-              ignore.case = ignore.case, perl = perl,
-              fixed = fixed, useBytes = useBytes)
-  if (is.null(requested_group) || requested_group == 0) {
-    result <- Map(function(x, m) h_regmatches(x, m)[[1]], 
-                  x, m)
-  } else if (length(m) > 0 &&
-             requested_group >= 1 &&
-             requested_group <= length(attr(m[[1]][[1]], 'capture.start')[1,])) {
-    # in following line, changed m to m[[1]] on 2020-11-11
-    # but switched back to m on 2021-07-29
-    result <- Map(function(x, m) h_regmatchesgroup(x, m,
-                                                   group = requested_group)[[1]], 
-                  x, m)
-  } else {
-    result <- rep(NA, length(x))
-  }
-  names(result) <- NULL
-  if (unlist) { result <- unlist(result) }
-  result
-}
-
-re_has_matches <- function(x, pattern, 
-                           ignore.case = FALSE, perl = TRUE,
-                           fixed = FALSE, useBytes = FALSE, 
-                           ...) {
-  grepl(pattern, x, ignore.case = ignore.case, perl = perl,
-        fixed = fixed, useBytes = useBytes)
-}
-
-re_which <- function(x, pattern, 
-                     ignore.case = FALSE, perl = TRUE,
-                     fixed = FALSE, useBytes = FALSE, 
-                     ...) {
-  grepl(pattern, x, ignore.case = ignore.case, perl = perl,
-        fixed = fixed, useBytes = useBytes)
-}
-
-re_replace_first <- function(x, pattern, replacement,
-                             ignore.case = FALSE, perl = TRUE,
-                             fixed = FALSE, useBytes = FALSE, 
-                             ...) {
-  sub(pattern, replacement, x, ignore.case = ignore.case, 
-      perl = perl, fixed = fixed, useBytes = useBytes)
-}
-
-re_replace_all <- function(x, pattern, replacement,
-                           ignore.case = FALSE, perl = TRUE,
-                           fixed = FALSE, useBytes = FALSE, ...) {
-  gsub(pattern, replacement, x, ignore.case = ignore.case, 
-       perl = perl, fixed = fixed, useBytes = useBytes)
-}
-
-# ----------------------------------------------------------------------------
-# constructor
-# ----------------------------------------------------------------------------
-
-re <- function(x, perl = TRUE, ...) {
-  # -- test x for errors
-  if (!"character" %in% class(x)) {
-    stop("x must be of the class 'character'")
-  }
-  if (is.na(x[1])) {
-    stop("x[1] must not be NA")
-  }
-  # -- test perl for errors
-  if (!is.logical(perl)) {
-    stop("perl must be a logical vector")
-  }
-  if (is.na(perl[1])) {
-    stop("perl[1] must not be NA")
-  }  
-  # -- test x for peculiarities we want to warn about
-  if (length(x) > 1) {
-    warning("x contains multiple items; only x[1] is used")
-  }
-  # -- test perl for peculiarities we want to warn about
-  if (length(perl) > 1) {
-    warning("perl contains multiple items; only perl[1] is used")
-  }
-  # -- test ... argument
-  extra_args <- names(list(...))
-  if ("invert" %in% extra_args) {
-    warning("unsupported invert argument is ignored")
-  }
-  # construct 're' object
-  result <- list(x = x[1], perl = perl[1])
-  class(result) <- "re"
-  # return result
-  result
-}
-
-as_re <- function(x, perl = TRUE, ...) {
-  re(x, perl = perl, ...)
-}
-
-# not a big fan of the following notation, but supporting it anyway
-as.re <- function(x, perl = TRUE, ...) {
-  re(x, perl = perl, ...)
-}
-
-print.re <- function(x, ...) {
-  if (!"re" %in% class(x)) {
-    stop("x must be an object of the class 're'")
-  }
-  cat("Regular expression (perl = ", x$perl, ")\n", sep = "")
-  cat("------------------\n")
-  cat(x$x, "\n")
-}
-
-plot.re <- function(x, ...) {
-  if (!"re" %in% class(x)) {
-    stop("x must be an object of the class 're'")
-  }
-  warning("there is no plot method for 're' objects; doing nothing")
-  invisible(NULL)
-}
-
-summary.re <- function(object, ...) {
-  if (!"re" %in% class(object)) {
-    stop("object must be an object of the class 're'")
-  }
-  result <- object
-  class(result) <- c("summary.re", "re")
-  result
-}
-
-print.summary.re <- function(x, ...) {
-  if (!"summary.re" %in% class(x)) {
-    stop("x must be an object of the class 'summary.re'")
-  }
-  cat("Regular expression (perl = ", x$perl, ")\n", sep = "")
-  cat("------------------\n")
-  cat(x$x, "\n")
-}
-
-plot.summary.re <- function(x, ...) {
-  if (!"summary.re" %in% class(x)) {
-    stop("x must be an object of the class 'summary.re'")
-  }
-  warning("there is no plot method for 'summary.re' objects; doing nothing")
-  invisible(NULL)
-}
-
-as.character.re <- function(x, ...) {
-  if (!"re" %in% class(x)) {
-    stop("x must be an object of the class 're'")
-  }
-  x$x
-}
-
-as_character.re <- function(x, ...) {
-  if (!"re" %in% class(x)) {
-    stop("x must be an object of the class 're'")
-  }
-  x$x
-}
-
-perl_flavor <- function(x) {
-  if (!"re" %in% class(x)) {
-    stop("x must be an object of the class 're'")
-  }
-  x$perl
-}
-
-`perl_flavor<-` <- function(x, value) {
-  if (!"re" %in% class(x)) {
-    stop("x must be an object of the class 're'")
-  }
-  x$perl <- value
-  x
-}
-
-# --
-# everything below this point need thorough testing
-# --
-
+# TO TEST ======================================================================
+# TODO everything below this point need thorough testing
 
 scan_txt <- function() {
   result <- ""
