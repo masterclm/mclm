1c6ed52ee32fbd91d9c73d968e2f04bb2869ad70
mariana <mariana.montes@kuleuven.be>
2022-08-13 19:03:20 +0200

    roxygenize find_xpath, even add example!

--- R/xpath.R
+++ R/xpath.R
@@ -1,7 +1,35 @@
-# =============================================================================
-# functions in support of XPath
-# =============================================================================
-
+#' Run XPath query
+#' 
+#' This function finds matches for an XPath query in a corpus.
+#'
+#' @param pattern An XPath query.
+#' @param x A corpus: an [`fnames`] object, a character vector of an XML source,
+#'   a list of documents already parsed with [XML::xmlParse()].
+#' @param handlers Optional collection of functions used to map the different
+#'   XML nodes to R objects.
+#' @param trim Logical. Whether to strip of whitespace from the begginng and end
+#'  of text strings.
+#' @param fun Optional function to be applied to the individual matches prior
+#'   to returning the result.
+#' @param final_fun Optional final function to be applied to the complete list
+#'  of matches prior to returning the result.
+#' @param namespaces Optional namespaces.
+#' @param ... Additional arguments.
+#'
+#' @return A list with matches (see [XML::xpathApply()]).
+#' @export
+#' 
+#' @examples 
+#' test_xml <- '
+#' <p>
+#'   <w pos="at">The</w>
+#'   <w pos="nn">example</w>
+#'   <punct>.</punct>
+#' </p>'
+#' 
+#' find_xpath("//w", test_xml)
+#' find_xpath("@pos", test_xml)
+#' find_xpath("//w[@pos='nn']", test_xml)
 find_xpath <- function(pattern,
                        x,
                        handlers = NULL,
@@ -10,7 +38,6 @@ find_xpath <- function(pattern,
                        final_fun = NULL,
                        namespaces = NULL,
                        ...) {
-   # ----------------------------------------------------------
    # - x can be any of the following:
    #      - a vector of filenames
    #      - a character vector of XML source
@@ -21,14 +48,13 @@ find_xpath <- function(pattern,
    #       then these arguments are ignored.
    #  - ...
    #       passed on to sapply
-   # ----------------------------------------------------------
    if (!is.vector(x)) {
       x <- list(x)
    }
    res <- vector("list", length(x))
    for (i in seq_along(x)) {
       cur <- x[[i]]
-      # -------------------------------------------------------
+      # --
       # - if cur turns out to be XML source or a filename,
       #   which is tested with (is.character(cur)), we
       #   parse it with xmlParse()
@@ -38,13 +64,12 @@ find_xpath <- function(pattern,
       #   however, currently this default case is not tested,
       #   just assumed.
       # case of cur.x is XML source or filename
-      # -------------------------------------------------------
+      # --
       if (is.character(cur)) { 
          cur <- XML::xmlParse(cur,
                               handlers = handlers,
                               trim = trim)
       }   
-      # -------------------------------------------------------
       #res[[i]] <- cur[pattern]
       if (is.null(namespaces)) {
          namespaces = XML::xmlNamespaceDefinitions(cur, simplify = TRUE)
